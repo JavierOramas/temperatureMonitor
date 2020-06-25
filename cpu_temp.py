77765081ceb8aaf164dd55d3a94aa2f95e0e9067
Javier Oramas <javiale2000@gmail.com>
2020-06-25 03:25:33 -0400

    export to json switched to psutil

--- cpu_temp.py
+++ cpu_temp.py
@@ -1,27 +1,12 @@
-from os import popen
 import psutil
+from json import dump
+import datetime
+# def get_cpu_temps():
+cpu = {}
+for i in psutil.sensors_temperatures()['coretemp']:
+    cpu[i[0]] = i[1]
 
-def get_cpu_temps():
-    temps = popen('sensors').read()
+cpu['date'] = str(datetime.datetime.now())
 
-    core_count = psutil.cpu_count(logical=False)
-
-    temps = temps.split('\n')
-
-    for i,item in enumerate(temps):
-        if 'Package id 0:' in item:
-            idx = i
-
-    cpu_temp = temps[idx]
-    cpu_cores = []
-
-    for i in range(core_count):
-        cpu_cores.append(temps[idx+i+1])
-        
-    return cpu_temp,cpu_cores
-
-f = open('temps.txt', 'a')
-cpu,cores = get_cpu_temps()
-f.write(cpu+'\n')
-for i in cores:
-    f.write(i+'\n')
+with open('temp.json', 'a') as json_file:
+    dump(cpu, json_file)    
