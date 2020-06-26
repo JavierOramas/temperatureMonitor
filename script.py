18bc317eaa24868b5f5697d61af11c0ae217ebb8
Javier Oramas <javiale2000@gmail.com>
2020-06-26 14:43:07 -0400

    float data fixed date fixed

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ from utils import list_disks,clean_digits
 import string
 # def get_cpu_temps():
 temps = {}
-temps['date'] = str(datetime.datetime.now())
+temps['date'] = str(datetime.datetime.now().strftime('%Y-%m-%d'))
 
 for i in psutil.sensors_temperatures()['coretemp']:
     temps[i[0]] = i[1]
@@ -17,10 +17,10 @@ list_hdds = list_disks()
 for i in list_hdds:
     text = str(popen('sudo hddtemp /dev/'+i).read())
     if text == '':
-        text = '0'
+        text = 0.0
     else:
         text = text.split(': ')[-1].replace('\u00b0C\n', '')
-        text = clean_digits(text)
+        text = float(clean_digits(text))
     temps[i] = text
 
 home = getenv("HOME")
