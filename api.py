92093549694d374de1c9f94233d597c2ef03bbdd
Javier Oramas <javiale2000@gmail.com>
2020-06-27 17:43:55 -0400

    added history data to api

--- api.py
+++ api.py
@@ -4,7 +4,12 @@ from os import path
 app = Flask(__name__)
 
 @app.route('/')
-def dummy_api():
+def get_temps():
     return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+@app.route('/history')
+def get_history():
+    return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
