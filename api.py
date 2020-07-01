97947c7a9d93bfd6f55e3c000d657601c2fb9b56
Javier Oramas <javiale2000@gmail.com>
2020-07-01 13:52:37 -0400

    core count added to api file

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
 from flask import Flask
 import pandas as pd
 from os import path
+import psutil
 app = Flask(__name__)
 
 @app.route('/temps')
 def get_temps():
-    return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+    json_file = pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+    json_file['n_cores'] = len(psutil.sensors_temperatures()['coretemp'])-2
+    return json_file
+    # return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+
 @app.route('/history')
 def get_history():
     return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
