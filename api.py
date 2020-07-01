8a84312fbba9f6dce7f6dad21f540c0c888e7627
Javier Oramas <javiale2000@gmail.com>
2020-07-01 13:59:08 -0400

    list disk added to api

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import pandas as pd
 from os import path
+from utils import list_disks
 import psutil
 app = Flask(__name__)
 
@@ -8,12 +9,16 @@ app = Flask(__name__)
 def get_temps():
     json_file = pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
     json_file['n_cores'] = len(psutil.sensors_temperatures()['coretemp'])-2
+    json_file['disks_name'] = list_disks()
     return json_file
     # return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
 
 @app.route('/history')
 def get_history():
-    return pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+    json_file = pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), lines=True, convert_dates=False, dtype=float).to_dict(orient='dict')
+    json_file['n_cores'] = len(psutil.sensors_temperatures()['coretemp'])-2
+    json_file['disks_name'] = list_disks()
+    return json_file
 
 
 if __name__ == '__main__':
