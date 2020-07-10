97b421124ffeb3acce79ca323d8462fdf2b9e3e1
Javier Oramas <javiale2000@gmail.com>
2020-07-10 01:02:44 -0400

    api catch network inteface exception

--- api.py
+++ api.py
@@ -33,8 +33,10 @@ def clean():
     
 
 if __name__ == '__main__':
-    ipv4 = popen('ip addr show wlo1').read().split("inet ")[1].split("/")[0]
-    #change wlo1 to your network adapter's name
-    app.run(host=ipv4, port=9998)
-    #change host and port to the values you find convenient
-    
+    try:
+        ipv4 = popen('ip addr show wlo1').read().split("inet ")[1].split("/")[0]
+        #change wlo1 to your network adapter's name
+        app.run(host=ipv4, port=9998)
+        #change host and port to the values you find convenient
+    except:
+        print("The network interface is not detected or not connected, \n please check network connection or chanche interface name in api.py" )
