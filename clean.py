c1b180e8c020ef4b9a06149ce2cfef0e6101ec74
Javier Oramas <javiale2000@gmail.com>
2020-09-25 12:47:50 -0400

    clean other json files as well

--- clean.py
+++ clean.py
@@ -7,29 +7,32 @@ from utils import filter_date
 from datetime import datetime
 
 def clean(items_left=100):
-    df = pd.read_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, convert_dates=False, dtype=float)
+    files = ['data/temps.json','data/memory_usage.json', 'data/cpu_usage.json']
 
-    # df['date'] = [i for i in df['date']]
-    if (len(df) < 100):
-        return
-    
-    ndf = pd.DataFrame(df.tail(items_left))
-    df = df[:-items_left]    
+    for i in files:
+        df = pd.read_json(path.join(path.dirname(path.abspath(__file__)),i), lines=True, convert_dates=False, dtype=float)
 
-    ndf.to_json(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), lines=True, orient='records')
-    with open(path.join(path.dirname(path.abspath(__file__)),'data/temp.json'), 'a') as json_file:
-        json_file.write('\n')
+        # df['date'] = [i for i in df['date']]
+        if (len(df) < items_left):
+            continue
 
-    if len(df) > 0:
-        df.to_json(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'), orient='records', lines=True), 
+        ndf = pd.DataFrame(df.tail(items_left))
+        df = df[:-items_left]    
 
-        df = open(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'), 'r')
-        with open(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), 'a') as json_file:
-            for i in df.readlines():
-                json_file.write(i)
-                if not i[-1] == '\n':
-                    json_file.write('\n')
+        ndf.to_json(path.join(path.dirname(path.abspath(__file__)),i), lines=True, orient='records')
+        with open(path.join(path.dirname(path.abspath(__file__)),i), 'a') as json_file:
+            json_file.write('\n')
 
-        remove(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'))
+        # if len(df) > 0:
+        #     df.to_json(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'), orient='records', lines=True), 
+
+        #     df = open(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'), 'r')
+        #     with open(path.join(path.dirname(path.abspath(__file__)),'data/history.json'), 'a') as json_file:
+        #         for i in df.readlines():
+        #             json_file.write(i)
+        #             if not i[-1] == '\n':
+        #                 json_file.write('\n')
+
+        #     remove(path.join(path.dirname(path.abspath(__file__)),'data/temporal.json'))
 if __name__ == '__main__':
     clean(int(sys.argv[1]))
